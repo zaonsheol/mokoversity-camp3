c9b4ebf91aa532123aff7e8eb12c3a2f8b819b88
zaonsheol <zaon996@gmail.com>
2013-10-20 14:00:22 +0800

    node.js installation, fix #3

--- main.js
+++ main.js
@@ -1,33 +1,65 @@
-var gameModule = (function() {
+/*jslint browser: true, devel: true, closure: true */
 
-        var timeoutVar,
-                counter = 0;
+var gameModule = (function () {
+
+
+    "use strict";
+
+var timeoutVar,
+                counter = 0,
+                ballX,
+                ballY,
+                ballR,
+                scores,
+                colors = ['#ff0000', '#0000ff', 'yellow'],
+                length = colors.length;
+
+        function touchEvent(evt) {
+                var x = evt.clientX,
+                        y = evt.clientY,
+                        tmp = (ballX - x) * (ballX - x) + (ballY - y) * (ballY - y);
+
+                console.log("Clicked: " + x + " , " + y);
+
+                if (tmp < ballR*ballR) {
+                        scores = scores + (100 - ballR);
+                        console.log("Hit ! Your scores: " + scores);
+                }
+        }
 
         function start() {
+                scores = 0;
+
+                document.getElementById("main").addEventListener("click", touchEvent, false);
+                startGame();                
+        }
+
+        function startGame() {
         var canvas = document.getElementById('game');
         var ctx = canvas.getContext('2d');
-            var ballX = Math.floor(Math.random() * 600); // 0..300
-            var ballY = Math.floor(Math.random() * 450);
-            var ballR = Math.floor(Math.random() * 80);
+            
+            ballX = Math.floor(Math.random() * 600); // 0..300
+            ballY = Math.floor(Math.random() * 450);
+            ballR = Math.floor(Math.random() * 80);
 
         canvas.width = 640;
         canvas.height = 480;
 
-        ctx.fillStyle = '#ff0000';
+        ctx.fillStyle = colors[counter % length];
         ctx.beginPath();
         ctx.arc(ballX, ballY, ballR, 0, Math.PI * 2 , true);
         ctx.fill();
 
         if (counter >= 10) {
-
+                gameOver();
         } else {
-                timeoutVar = setTimeout(start, 1000);
+                timeoutVar = setTimeout(startGame, 2000);
                 counter = counter + 1;
             } 
         }
 
         function gameOver() {
-        console.log("Counter: " + counter);
+        console.log("Final: " + scores);
         }
 
         return {
---
+++ routes/user.js
@@ -0,0 +1,12 @@
+
+/*
+ * GET users listing.
+ */
+
+exports.list = function(req, res){
+  res.send("respond with a resource");
+};
+
+exports.save = function(req, res){
+  console.log("your scores is ");
+};
